6dd66c6e4d1af72ff440c5e7c5f633a74ae4b51f
Brian Yu <brianyu28@gmail.com>
2016-07-04 15:21:21 -0400

    Updated home blueprint files

--- controllers/home.py
+++ controllers/home.py
@@ -6,3 +6,24 @@ home = Blueprint('home', __name__,
 @home.route('/')
 def homepage():
    return render_template('homepage.html')
+
+@home.route('/login/')
+def login():
+    return render_template('login.html')
+
+@home.route('/register/')
+def register():
+    return render_template('register.html')
+
+@home.route('/about/')
+def about():
+    return render_template('about.html')
+
+@home.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+@home.route('/getstarted/')
+def getstarted():
+    return render_template('getstarted.html')
+
