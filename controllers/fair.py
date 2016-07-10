ccf4ef1d4826a6349de2b0e9ed770cab355a5724
Brian Yu <brianyu28@gmail.com>
2016-07-10 11:38:15 -0700

    added date picker for jQuery UI

--- controllers/fair.py
+++ controllers/fair.py
@@ -4,6 +4,10 @@ from model import helpers, dbmain
 fair = Blueprint('fair', __name__,
                         template_folder='../templates/fair')
 
+@fair.route('/')
+def fair_page():
+    return redirect(url_for('fair.manage'))
+
 @fair.route('/manage/')
 def manage():
     return render_template('manage.html', user=dbmain.currentUser())
