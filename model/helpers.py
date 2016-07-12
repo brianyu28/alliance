56d54eab7bf221cff549ca8e210874cb34251479
Brian Yu <brianyu28@gmail.com>
2016-07-12 00:57:55 -0400

    changed password hashing to Flask native hashing

--- model/helpers.py
+++ model/helpers.py
@@ -1,7 +1,7 @@
-import bcrypt
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def get_hashed_password(plain_text_password):
-    return bcrypt.hashpw(plain_text_password, bcrypt.gensalt(8))
+    return generate_password_hash(plain_text_password)
 
 def check_password(plain_text_password, hashed_password):
-    return bcrypt.checkpw(plain_text_password, hashed_password)
+    return check_password_hash(hashed_password, plain_text_password)
